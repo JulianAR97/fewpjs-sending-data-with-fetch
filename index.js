62f92573380d52dde48a273506049500dd2101b4
Julian <julianrosenthal@yahoo.com>
2021-02-25 16:48:46 -0700

    commit tree f46b9e5b424f418aa362664141c0f4734e5dcac4

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email) {
+  let formData = {
+    name: name,
+    email: email
+  }
+
+  let configObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'accept': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  }
+
+  return fetch('http://localhost:3000/users', configObj)
+    .then(resp => resp.json())
+    .then(json => appendId(json))
+    .catch(error => appendError(error))
+}
+
+
+function appendId(json) {
+  document.body.innerHTML = json.id
+}
+
+function appendError(error) {
+  document.body.innerHTML = error.message
+}
